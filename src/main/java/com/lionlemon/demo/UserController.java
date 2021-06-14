8a2b6f8598f836fb497a64d6e14cc3dd9c90952d
lionlemon <ame9807@gmail.com>
2021-06-15 03:21:16 +0900

    springDemo 第9次测试 有参构造方法的依赖注入

--- src/main/java/com/lionlemon/demo/UserController.java
+++ src/main/java/com/lionlemon/demo/UserController.java
@@ -2,18 +2,17 @@ package com.lionlemon.demo;
 
 
 import com.lionlemon.service.UserService;
-import com.lionlemon.service.impl.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class UserController {
     public static void main(String[] args) {
-        /*
-         * ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-         * UserService userService = (UserService) applicationContext.getBean("userService");
-         * userService.save();
-         */
-        //空指针异常 因为自己New出来的对象没有值。
-        UserService userService = new UserServiceImpl();
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) applicationContext.getBean("userService");
         userService.save();
+
+
     }
 }
--- src/main/java/com/lionlemon/service/impl/UserServiceImpl.java
+++ src/main/java/com/lionlemon/service/impl/UserServiceImpl.java
@@ -9,8 +9,11 @@ public class UserServiceImpl implements UserService {
 
     private UserDao userDao;
 
-    public void setUserDao(UserDao userDao) {
+    public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
+    }
+
+    public UserServiceImpl() {
     }
 
     @Override
