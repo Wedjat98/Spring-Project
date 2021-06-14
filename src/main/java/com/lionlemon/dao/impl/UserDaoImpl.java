7b11c49249f564523f812a8a42c910e5b7d92729
lionlemon <ame9807@gmail.com>
2021-06-15 00:53:10 +0900

    springDemo 第一次测试

---
+++ src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
@@ -0,0 +1,11 @@
+package com.lionlemon.dao.impl;
+
+import com.lionlemon.dao.UserDao;
+
+public class UserDaoImpl implements UserDao {
+
+    @Override
+    public void save() {
+        System.out.println("saving...............");
+    }
+}
