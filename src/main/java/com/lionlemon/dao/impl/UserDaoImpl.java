4c43013bddf2c5123e48f1aba8d5bca3841bbca7
lionlemon <ame9807@gmail.com>
2021-06-15 03:32:51 +0900

    springDemo 第10次测试 普通属性的依赖注入

--- src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
+++ src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
@@ -3,22 +3,20 @@ package com.lionlemon.dao.impl;
 import com.lionlemon.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
+    private String username;
+    private int age;
 
-    public UserDaoImpl() {
-        System.out.println("UserDaoImpl对像被创建............");
+    public void setUsername(String username) {
+        this.username = username;
     }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-    public void init(){
-        System.out.println("初始化方法。。。。。。。");
-    }
-    public void destroy(){
-        System.out.println("销毁方法。。。。。。。");
-    }
     @Override
     public void save() {
-
-
+        System.out.println(username+"---------"+age);
         System.out.println("save method is running...............");
     }
 }
