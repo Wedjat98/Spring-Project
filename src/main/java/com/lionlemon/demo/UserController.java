28ed699abf447d55d463b06b5d398b04c2f6a388
lionlemon <ame9807@gmail.com>
2021-06-15 02:54:02 +0900

    springDemo 第8次测试 get方法的依赖注入（自己new对象是会空指针异常的）

--- src/main/java/com/lionlemon/demo/UserController.java
+++ src/main/java/com/lionlemon/demo/UserController.java
@@ -2,13 +2,18 @@ package com.lionlemon.demo;
 
 
 import com.lionlemon.service.UserService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.lionlemon.service.impl.UserServiceImpl;
+
 
 public class UserController {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userService = (UserService) applicationContext.getBean("userService");
+        /*
+         * ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+         * UserService userService = (UserService) applicationContext.getBean("userService");
+         * userService.save();
+         */
+        //空指针异常 因为自己New出来的对象没有值。
+        UserService userService = new UserServiceImpl();
         userService.save();
     }
 }
