acb6494ed37c927acf4cf7c14876347a57bcaf82
lionlemon <ame9807@gmail.com>
2021-06-15 01:38:27 +0900

    springDemo 第3次测试 测试scope属性

---
+++ src/test/java/com/lionlemon/test/SpringTest.java
@@ -0,0 +1,19 @@
+package com.lionlemon.test;
+
+import com.lionlemon.dao.UserDao;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringTest {
+    @Test
+    //测试scope属性
+    public void test1(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserDao userDao1 = (UserDao) app.getBean("userDao");
+        UserDao userDao2 = (UserDao) app.getBean("userDao");
+        System.out.println(userDao1);
+        System.out.println(userDao2);
+    }
+
+}
