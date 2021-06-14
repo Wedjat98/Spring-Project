d1090e3eec220c7ea97023ba923bdfcb61a0f2e3
lionlemon <ame9807@gmail.com>
2021-06-15 01:44:26 +0900

    springDemo 第4次测试 测试无参构造方法被使用一次了

--- src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
+++ src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
@@ -4,8 +4,14 @@ import com.lionlemon.dao.UserDao;
 
 public class UserDaoImpl implements UserDao {
 
+    public UserDaoImpl() {
+        System.out.println("UserDaoImpl对像被创建............");
+    }
+
     @Override
     public void save() {
+
+
         System.out.println("save method is running...............");
     }
 }
