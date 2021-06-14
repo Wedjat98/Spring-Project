d5cf36980a749aaa88bf21992d74c055d7894927
lionlemon <ame9807@gmail.com>
2021-06-15 04:08:46 +0900

    第10次测试 集合的依赖注入

--- src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
+++ src/main/java/com/lionlemon/dao/impl/UserDaoImpl.java
@@ -1,8 +1,30 @@
 package com.lionlemon.dao.impl;
 
 import com.lionlemon.dao.UserDao;
+import com.lionlemon.domain.User;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class UserDaoImpl implements UserDao {
+
+    private List<String> stringList;
+    private Map<String, User>userMap;
+    private Properties properties;
+
+    public void setStringList(List<String> stringList) {
+        this.stringList = stringList;
+    }
+
+    public void setUserMap(Map<String, User> userMap) {
+        this.userMap = userMap;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
     private String username;
     private int age;
 
@@ -16,7 +38,10 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void save() {
-        System.out.println(username+"---------"+age);
+//        System.out.println(username+"---------"+age);
+        System.out.println(stringList);
+        System.out.println(properties);
+        System.out.println(userMap);
         System.out.println("save method is running...............");
     }
 }
--- src/main/java/com/lionlemon/demo/UserController.java
+++ src/main/java/com/lionlemon/demo/UserController.java
@@ -8,11 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserController {
     public static void main(String[] args) {
-
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService = (UserService) applicationContext.getBean("userService");
         userService.save();
-
-
     }
 }
