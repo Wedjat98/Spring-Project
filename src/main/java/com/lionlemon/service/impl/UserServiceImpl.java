e8758cf5cfe1d8ee3e8d61b7e7f618c3eff893e0
lionlemon <ame9807@gmail.com>
2021-06-15 02:49:40 +0900

    springDemo 第8次测试 get方法的依赖注入

--- src/main/java/com/lionlemon/service/impl/UserServiceImpl.java
+++ src/main/java/com/lionlemon/service/impl/UserServiceImpl.java
@@ -6,10 +6,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserServiceImpl implements UserService {
+
+    private UserDao userDao;
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public void save() {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao userDao = (UserDao) app.getBean("userDao");
         userDao.save();
     }
 }
