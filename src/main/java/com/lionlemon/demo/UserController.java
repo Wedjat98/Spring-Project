5d75474c5ac02b284d576b5ef3db6cdd65bad770
lionlemon <ame9807@gmail.com>
2021-06-15 09:43:22 +0900

    第13次测试 ApplicationContext和字节码文件获取对象

--- src/main/java/com/lionlemon/demo/UserController.java
+++ src/main/java/com/lionlemon/demo/UserController.java
@@ -3,13 +3,17 @@ package com.lionlemon.demo;
 
 import com.lionlemon.service.UserService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 
 public class UserController {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserService userService = (UserService) applicationContext.getBean("userService");
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //通过绝对路径获取
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("C:\\Users\\ame98\\IdeaProjects\\helloSpring\\src\\main\\resources\\applicationContext.xml");
+//        UserService userService = (UserService) applicationContext.getBean("userService");
+        //字节码文件获取 只限于一个实现类的单个对象 多个对象时会报错
+        UserService userService = applicationContext.getBean(UserService.class);
         userService.save();
     }
 }
